9083b989c6d481c3071c5d5cbfd165bbc849af82
XiaoNaoXu <1983330788@qq.com>
2020-06-17 22:06:02 +0800

    xiugai

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import shutil
 root_pwd = os.getcwd()
 s_path = root_pwd + '/data/'
 d_path = root_pwd + '/result/'
-# while True:
-#     pass
+while True:
+    pass
 if os.path.isdir(s_path):
     for files in os.listdir(s_path):
         try:
